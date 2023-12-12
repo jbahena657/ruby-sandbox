26647519fe46861e56c73c3e4ad5744860dd6371
jbahena657 <143520555+jbahena657@users.noreply.github.com>
2023-12-12 15:38:15 +0000

    added gems

--- howdy.rb
+++ howdy.rb
@@ -1,4 +1,12 @@
-# here's a comment
+# /howdy.rb
+source "https://rubygems.org"
+
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
+require "active_support/all"
+
 my_string = "Hello, world!"
 pp my_string
 
+require "./goodbye.rb"
